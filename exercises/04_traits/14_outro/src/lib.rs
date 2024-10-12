5ad6728110feeac27ad09d971970a27ddecc6e61
machida <machida@kmc.gr.jp>
2024-10-13 01:04:21 +0900

    04_traits outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -9,10 +9,15 @@
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
 
-struct SaturatingU16 {
-    value: u16,
+use std::ops::Add;
+
+//   It should hold a `u16` value.
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct SaturatingU16 {
+    pub value: u16,
 }
 
+//   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
 impl From<u16> for SaturatingU16 {
     fn from(value: u16) -> Self {
         SaturatingU16 { value: value }
@@ -38,5 +43,50 @@ impl From<&u8> for SaturatingU16 {
         SaturatingU16 {
             value: *value as u16,
         }
+    }
+}
+
+//   It should support addition with a right-hand side of type
+//   SaturatingU16, u16, &u16, and &SaturatingU16. Addition should saturate at the
+//   maximum value for `u16`.
+impl Add for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        self + rhs.value
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs),
+        }
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self::Output {
+        self + *rhs
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        self + rhs.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+
+impl PartialOrd<u16> for SaturatingU16 {
+    fn partial_cmp(&self, other: &u16) -> Option<std::cmp::Ordering> {
+        self.value.partial_cmp(other)
     }
 }
